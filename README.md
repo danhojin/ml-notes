9f3fccf3bf49f085533259ba7e9936e85ee6dee9
danhojin <danhojin@gmail.com>
2020-02-10 05:14:43 +0900

    rename folders

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ dl/mnist_digit_pairs_keras_shared_vision_model.ipynb
 * mnist에서 두 sample을 골랐을 때 같은 수인지 아니지 판별
 * colab 환경인 경우 반드시 GPU 사용
 
-dl/carpole_ray
+dl/cartpole_gray
 
-* ray 환경에서 carpole 컨트롤 정책 수립
+* wrapped env to capture gray images
 
-dl/acrobot_moutaincar_ray_multidiscrete.py
+dl/cartpoles
 
 * gym.spaces.MultiDiscrete action test in the rllib environment
-* only work for vanilla PG for now
--- dl/cartpoles/envs/cartpoles_multidiscrete_action.py
+++ dl/cartpoles/envs/cartpoles_multidiscrete_action.py
@@ -2,12 +2,12 @@ import numpy as np
 import gym
 from gym import spaces
 
-import ray
-from ray import tune
-from ray.tune.registry import register_env
-
-
-ray.init(ignore_reinit_error=True, log_to_driver=False)
+# import ray
+# from ray import tune
+# from ray.tune.registry import register_env
+# 
+# 
+# ray.init(ignore_reinit_error=True, log_to_driver=False)
 
 
 class CustomEnv(gym.Env):
@@ -19,45 +19,38 @@ class CustomEnv(gym.Env):
     def __init__(self, env_config=None):
         self.env_config = env_config or {}
         super().__init__()
-        self.envs = [gym.make('Acrobot-v1'), gym.make('MountainCar-v0')]
-        n = []
-        obs_low = []
-        obs_high = []
-        for env in self.envs:
-            n.append(env.action_space.n)
-            obs_low.append(env.observation_space.low)
-            obs_high.append(env.observation_space.high)
-        obs_low = np.concatenate(obs_low)
-        obs_high = np.concatenate(obs_high)
-        self.observation_space = spaces.Box(obs_low, obs_high)
-        self.action_space = spaces.MultiDiscrete(n)
-        self.num_dones = 0
+        self.envs = [gym.make('CartPole-v1'), gym.make('CartPole-v1'), gym.make('CartPole-v1')]
+        action_space_ns = []
+        observation_spaces = {}
+        for i, env in enumerate(self.envs):
+            action_space_ns.append(env.action_space.n)
+            observation_spaces[i] = env.observation_space
+
+        self.observation_space = spaces.Dict(observation_spaces)
+        self.action_space = spaces.MultiDiscrete(action_space_ns)
 
     def reset(self):
-        self.num_dones = 0
-        obs = []
-        for env in self.envs:
-            obs.append(env.reset())
-        return np.concatenate(obs)
+        obs = {}
+        for i, env in enumerate(self.envs):
+            obs[i] = env.reset()
+        return spaces.Dict(obs)
 
     def step(self, actions):
         actions = list(actions)  # in case of numpy actions
 
         obs = []
         rewards = []
+        envs_done = False
         for env, action in zip(self.envs, actions):
             env_obs, env_reward, env_done, _ = env.step(action)
-            if env_done:
-                self.num_dones += 1
-                env_reward += 10
-                env_obs = env.reset()
+            envs_done = envs_done or env_done
             obs.append(env_obs)
             rewards.append(env_reward)
 
         obs = np.concatenate(obs)
         rewards = sum(rewards)
 
-        return obs, rewards, True if self.num_dones == 2 else False, {}
+        return obs, rewards, envs_done, {}
 
     def close(self):
         for env in self.envs:
